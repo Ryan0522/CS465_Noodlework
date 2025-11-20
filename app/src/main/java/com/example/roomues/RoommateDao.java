7d33c6424d898dfe257a7e812ef0636e0f24b38e
ethanmathw <ethmth@gmail.com>
2025-11-20 20:53:33 +0100

    Added Delete

--- app/src/main/java/com/example/roomues/RoommateDao.java
+++ app/src/main/java/com/example/roomues/RoommateDao.java
@@ -1,19 +1,38 @@
 package com.example.roomues;
 
 import androidx.room.Dao;
+import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Query;
+import androidx.room.Update;
 import androidx.lifecycle.LiveData;
+
 import java.util.List;
 
 @Dao
 public interface RoommateDao {
+
+    // INSERT
     @Insert
     void insert(RoommateEntity roommate);
 
+    // UPDATE
+    @Update
+    void update(RoommateEntity roommate);
+
+    // DELETE ONE
+    @Delete
+    void delete(RoommateEntity roommate);
+
+    // DELETE ALL
+    @Query("DELETE FROM roommates")
+    void deleteAll();
+
+    // GET ALL (LIST)
     @Query("SELECT * FROM roommates")
     List<RoommateEntity> getAllRoommates();
 
+    // GET ALL LIVE DATA (for auto-updates)
     @Query("SELECT * FROM roommates")
     LiveData<List<RoommateEntity>> getAllRoommatesLive();
 }
