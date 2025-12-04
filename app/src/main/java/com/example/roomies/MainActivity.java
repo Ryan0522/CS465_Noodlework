33ef1f0f390f79fb17d8b80a11fcabf1d6e0cda7
jiaweis7 <jiaweis7@illinois.edu>
2025-12-04 02:10:13 -0600

    Upload refined images

--- app/src/main/java/com/example/roomies/MainActivity.java
+++ app/src/main/java/com/example/roomies/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Log.d("INFO","Entrance");
+//        Log.d("INFO","Entrance");
         SharedPreferences prefs = getSharedPreferences("app_prefs", MODE_PRIVATE);
         boolean isFirstRun = prefs.getBoolean("first_run", true);
 
